1527ebe21569ef50a33c4897cc0ecf55b1a8dfff
Ivan Stepanov <ivan.stepanov08.1999@gmail.com>
2024-09-24 22:19:15 -0400

    invalid number err

--- CsvReader.java
+++ CsvReader.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class CsvReader{
 
@@ -8,16 +12,20 @@ public class CsvReader{
             while((line = br.readLine()) != null){
 
                 String[] values = line.split(",");
-
+            try {
                 BigDecimal income = new BigDecimal(values[0]);
                 BigDecimal expenses = new BigDecimal(values[1]);
                 BigDecimal debtLoans = new BigDecimal(values[2]);
                 BigDecimal interest = new BigDecimal(values[3]);
 
                 String typeOfLoans = values[4];
+            } catch(NumberFormatException e){
+                System.err.println("Invalid number format in line " + line);
+                e.printStackTrace();
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace;
+            e.printStackTrace();
         }
 
     }
--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args){
-        String filePath = "";
-        CsvReader.readCsv(ilePath);
+        String filePath = "CSV.txt";
+        CsvReader reader = new CsvReader();
+
+        reader.readCsv(filePath);
+
+
+
     }
 }
