9ffe171b6d507cbc366d3a0ef8f78f7ea756824c
Ivan Stepanov <ivan.stepanov08.1999@gmail.com>
2024-09-26 23:03:11 -0400

    CsvWriter class added

---
+++ CsvWriter.java
@@ -0,0 +1,43 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class CsvWriter {
+    public static void writeToCsv(String path){
+        try (Scanner keyboard = new Scanner(System.in);
+             PrintWriter pw = new PrintWriter(new FileWriter(path, true))){
+
+            String toContinue;
+
+            do{
+                System.out.println("Please enter income: ");
+                BigDecimal income = new BigDecimal(keyboard.nextLine());
+
+                System.out.println("Please enter expense: ");
+                BigDecimal expense = new BigDecimal(keyboard.nextLine());
+
+                System.out.println("Please enter debt/loan: ");
+                BigDecimal debtLoan = new BigDecimal(keyboard.nextLine());
+
+                System.out.println("Please enter interest ");
+                BigDecimal interest = new BigDecimal(keyboard.nextLine());
+
+                System.out.println("Please enter type of loan ");
+                String typeOfLoan = keyboard.nextLine();
+
+                pw.println(income+","+expense+","+debtLoan+","+interest+","+typeOfLoan);
+
+                System.out.println("Would you like to continue? yes/no:");
+                toContinue = keyboard.nextLine();
+
+            }while (toContinue.equalsIgnoreCase("yes"));
+
+            System.out.println("Data sucssesfully added to the " + path);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main{
     public static void main(String[] args){
         String filePath = "CSV.txt";
+
         CsvReader reader = new CsvReader();
+        CsvWriter writer = new CsvWriter();
+
         CalculationCsv calculation = new CalculationCsv();
 
         reader.readCsv(filePath);
+        writer.writeToCsv(filePath);
+
 
         System.out.println(calculation.averageInterestRate(filePath)+"%");
 
